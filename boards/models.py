07f6ecb3ba4101cadf3ccc572c7336a1a1a46a4b
Cory <cory.meikle94@gmail.com>
2019-12-18 11:26:16 +0000

    Added requirements.txt. Removed bleach from models.py

--- boards/models.py
+++ boards/models.py
@@ -3,7 +3,6 @@ from django.utils.text import slugify
 from django.contrib.auth.models import User
 from django.utils import timezone
 from datetime import timedelta
-import bleach
 
 class ForumBoard(models.Model):
     
