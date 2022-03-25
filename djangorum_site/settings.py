f5e978e6316d995464ee3b023113c1821735394b
Cory <cory.meikle94@gmail.com>
2022-03-25 21:35:06 +0000

    WIP

--- djangorum_site/settings.py
+++ djangorum_site/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'assets')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -28,8 +28,7 @@ SECRET_KEY = '98#zjvoa+k+pio-%dj2#yv263&oi3ej&_!!t4n#z+n2%#937(!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangorum.herokuapp.com']
-
+ALLOWED_HOSTS = ['djangorum.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -53,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangorum_site.urls'
